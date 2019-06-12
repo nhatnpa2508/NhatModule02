cea2a687e05fa9d463bbc037ab34d2b845fadaad
nhatnpa2508 <49977249+nhatnpa2508@users.noreply.github.com>
2019-06-12 17:17:38 +0700

    update

---
+++ 9. Design and Create Database/GroupBy.sql
@@ -0,0 +1,24 @@
+/*select status from orders
+group by status;
+select status, count(*) as "So luong status"
+from orders
+group by status;
+select status, sum (quantityOrdered) as amount
+from classicmodels.orders
+inner join orderdetails on orders.orderNumber = orderdetails.orderNumber
+group by status
+select orderNumber, sum(quantityOrdered * priceEach) as total
+from orderdetails
+group by orderNumber;
+*/
+select status, sum(quantityOrdered * priceEach) as amount
+from orders 
+inner join orderdetails on orders.ordernumber = orderdetails.ordernumber
+group by status;
+
+select year(orderDate) as year, sum(quantityOrdered * priceEach) as total
+from orders 
+inner join orderdetails on orders.orderNumber = orderdetails.orderNumber
+where status = 'shipped'
+group by year
+having year > 2003

5f51a86c18aa2e8cb69ea8806d890c307b27259d
nhatnpa2508 <49977249+nhatnpa2508@users.noreply.github.com>
2019-07-19 17:19:37 +0700

    Add files via upload

---
+++ 16. Database and ORM/CustomerManager/src/main/java/com/codegym/cms/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+/*
+ *************************************
+ * Created by IntelliJ IDEA          *
+ * User: Nhat                     *
+ * Email: nhatnpa2508@gmail.com      *
+ * Date: 7/19/2019                     *
+ * Time: 2:44 PM                     *
+ *************************************
+ */
+
+package com.codegym.cms.repository;
+
+import com.codegym.cms.model.Customer;
+
+public interface CustomerRepository extends Repository<Customer> {
+}
---
+++ 16. Database and ORM/CustomerManager/src/main/java/com/codegym/cms/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,44 @@
+/*
+ *************************************
+ * Created by IntelliJ IDEA          *
+ * User: Nhat                     *
+ * Email: nhatnpa2508@gmail.com      *
+ * Date: 7/19/2019                     *
+ * Time: 2:46 PM                     *
+ *************************************
+ */
+
+package com.codegym.cms.service.impl;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.repository.CustomerRepository;
+import com.codegym.cms.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService{
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.remove(id);
+    }
+}

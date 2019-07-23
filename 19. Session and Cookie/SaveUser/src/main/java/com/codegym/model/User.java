efe34413f74f7e4ec488da9d142f638992c421b0
ngo phan anh nhat <nhatnpa2508@gmail.com>
2019-07-23 16:54:32 +0700

    update

---
+++ 19. Session and Cookie/SaveUser/src/main/java/com/codegym/model/User.java
@@ -0,0 +1,40 @@
+/*
+ *************************************
+ * Created by IntelliJ IDEA          *
+ * User: Nhat                     *
+ * Email: nhatnpa2508@gmail.com      *
+ * Date: 7/23/2019                     *
+ * Time: 4:20 PM                     *
+ *************************************
+ */
+
+package com.codegym.model;
+
+public class User {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}

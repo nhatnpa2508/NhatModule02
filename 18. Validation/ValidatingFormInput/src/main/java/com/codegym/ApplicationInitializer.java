865fa28fac228a43e66c6efac25710148a9de495
ngo phan anh nhat <nhatnpa2508@gmail.com>
2019-07-23 11:34:56 +0700

    update

---
+++ 18. Validation/ValidatingFormInput/src/main/java/com/codegym/ApplicationInitializer.java
@@ -0,0 +1,14 @@
+/*
+ *************************************
+ * Created by IntelliJ IDEA          *
+ * User: Nhat                     *
+ * Email: nhatnpa2508@gmail.com      *
+ * Date: 7/23/2019                     *
+ * Time: 11:18 AM                     *
+ *************************************
+ */
+
+package com.codegym;
+
+public class ApplicationInitializer {
+}
---
+++ 18. Validation/ValidatingFormInput/src/main/java/com/codegym/controller/UserController.java
@@ -0,0 +1,14 @@
+/*
+ *************************************
+ * Created by IntelliJ IDEA          *
+ * User: Nhat                     *
+ * Email: nhatnpa2508@gmail.com      *
+ * Date: 7/23/2019                     *
+ * Time: 11:20 AM                     *
+ *************************************
+ */
+
+package com.codegym.controller;
+
+public class UserController {
+}

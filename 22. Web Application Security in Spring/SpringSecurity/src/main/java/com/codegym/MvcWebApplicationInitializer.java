e2e34da0d5c473282ddb941a2f9abfd3b5311fc7
ngo phan anh nhat <nhatnpa2508@gmail.com>
2019-07-26 16:57:11 +0700

    update

---
+++ 22. Web Application Security in Spring/SpringSecurity/src/main/java/com/codegym/MvcWebApplicationInitializer.java
@@ -0,0 +1,30 @@
+/*
+ *************************************
+ * Created by IntelliJ IDEA          *
+ * User: Nhat                     *
+ * Email: nhatnpa2508@gmail.com      *
+ * Date: 7/26/2019                     *
+ * Time: 4:30 PM                     *
+ *************************************
+ */
+
+package com.codegym;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { WebSecurityConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
